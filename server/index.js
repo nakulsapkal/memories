90d684bb592dd5ef141666a0ec7b10ddfec00590
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-09-09 18:31:21 -0400

    server re commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var mongoose = require("mongoose");
+var morgan = require("morgan");
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(morgan("dev"));
+
+app.use(cors());
+
+//
+const CONNECTION_URL =
+  "mongodb+srv://nakulsapkal:nakulsapkal123@cluster0.dogfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
+  )
+  .catch((error) => console.log("Error connecting database:", error));
+
+mongoose.set("useFindAndModify", false);
