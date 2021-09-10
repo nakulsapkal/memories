ede01a6f6c604a9bff56f9d8370aacb6b776fb80
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-09-10 00:19:13 -0400

    add basic files and design and Image

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Memories from "../src/images/memories.png";
+import Posts from "../src/components/Posts/Posts";
+import Form from "../src/components/Form/Form";
 function App() {
   return (
-    <Router>
-      <div></div>
-    </Router>
+    // <Router>
+    //   <div className="App">
+    //     <Switch>
+    //       <Route path="/" />
+    //     </Switch>
+    //   </div>{" "}
+    // </Router>
+
+    <Container maxidth="lg">
+      <AppBar position="static">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={Memories} alt="Memories" height="60" />
+      </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignitems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Post from "../Posts/Post/Post";
+
+export default function Posts() {
+  return (
+    <>
+      <Post />
+    </>
+  );
+}
